31cb9136c5cda7dcb5cee0e1d779212d4d74b7a1
Joel Carlos <joel.carlos@merkle.com>
2025-07-09 20:53:59 +0800

    public folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-    resolve: {
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/**/*", // source files
+          dest: "public", // destination inside dist/
+        },
+      ],
+    }),
+  ],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
 });
