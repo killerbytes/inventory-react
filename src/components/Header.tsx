d1cb074871f01b006985986bff731e91f6bde952
Joel Carlos <joel.carlos@merkle.com>
2025-12-18 15:04:47 +0800

    get title from env

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <h1 className="container mx-auto flex items-center p-4 gap-2 font-semibold text-2xl">
-        <Store /> My Hardware
+        <Store /> {import.meta.env.VITE_APP_TITLE}
       </h1>
     </header>
   );
